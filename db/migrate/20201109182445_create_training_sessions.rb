05142cef654f126698d4a59d7ef78872eae0022b
Rizzooo <ronburrell05@gmail.com>
2020-11-09 13:27:07 -0500

    activeadmin login basic login functional

---
+++ db/migrate/20201109182445_create_training_sessions.rb
@@ -0,0 +1,15 @@
+class CreateTrainingSessions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :training_sessions do |t|
+      t.integer :trainer_id
+      t.integer :user_id
+      t.boolean :open
+      t.boolean :confirmed
+      t.datetime :start_time
+      t.datetime :end_time
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
